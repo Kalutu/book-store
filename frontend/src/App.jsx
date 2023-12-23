566a07206b7e8a861fcdaa291bf89f032369a37f
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-12-23 15:22:46 +0300

    setup routes with react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import {Route, Routes} from 'react-router-dom'
-import Home from './pages/Home'
-import CreateBook from './pages/CreateBook'
-import ShowBook from './pages/ShowBook'
-import EditBook from './pages/EditBook'
-import DeleteBook from './pages/DeleteBook'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import CreateBook from "./pages/CreateBook";
+import ShowBook from "./pages/ShowBook";
+import EditBook from "./pages/EditBook";
+import DeleteBook from "./pages/DeleteBook";
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/books/create' element={<CreateBook/>} />
-      <Route path='/books/details/:id' element={<ShowBook/>} />
-      <Route path='/books/edit/:id' element={<EditBook/>} />
-      <Route path='/books/delete/:id' element={<DeleteBook/>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/details/:id" element={<ShowBook />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
