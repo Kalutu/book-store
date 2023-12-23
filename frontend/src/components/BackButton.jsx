82af93a7b58f7bec0e6745792b64ae18e1625de3
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-12-23 15:20:07 +0300

    Create Spinner and BackButton components

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { BsArrowLeft } from 'react-icons/bs'
+import React from "react";
+import { Link } from "react-router-dom";
+import { BsArrowLeft } from "react-icons/bs";
 
-const BackButton = ({destination = '/'}) => {
+const BackButton = ({ destination = "/" }) => {
   return (
-    <div className='flex'>
-        <Link 
-            to={destination}
-            className='bg-sky-800 text-white px-4 py-1 rounded-lg w-fit'
-        >
-            <BsArrowLeft className='text-2xl'/>
-        </Link>
+    <div className="flex">
+      <Link
+        to={destination}
+        className="bg-sky-800 text-white px-4 py-1 rounded-lg w-fit"
+      >
+        <BsArrowLeft className="text-2xl" />
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default BackButton
+export default BackButton;
