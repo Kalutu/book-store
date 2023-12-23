bb1bb74ad081dd40a8c9875a5dc1b7811c9c1190
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-12-23 08:15:31 +0300

    Allow CORS

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,17 @@ import express from "express"
 import mongoose from "mongoose"
 import { PORT, MONGO_URI } from "./config.js"
 import { router as bookRouter } from "./routes/book.js"
+import cors from 'cors'
  
 const app = express()
+// app.use(cors('*'))
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type']
+    })
+)
 
 app.use(express.json())
 
